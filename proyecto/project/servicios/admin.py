6078669296a4e6c0b94a92fac3ed84383cdd128b
Lisandro Leone <leonelisandro@gmail.com>
2024-06-05 12:06:39 -0300

    app servicios

--- proyecto/project/servicios/admin.py
+++ proyecto/project/servicios/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from servicios.models import Servicios
+from servicios.models import Servicios, Funciones
 
 class ServicioAdmin(admin.ModelAdmin):
     readonly_fields = ('created','updated')
 
 admin.site.register(Servicios, ServicioAdmin)
+admin.site.register(Funciones)
--- proyecto/project/servicios/models.py
+++ proyecto/project/servicios/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+class Funciones(models.Model):
+    funcion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.funcion
+    
+    
 class Servicios(models.Model):
     titulo = models.CharField(max_length=50)
-    contenido = models.CharField(max_length=50)
+    contenido = models.ManyToManyField(Funciones)
     imagen = models.ImageField(null=True,blank=True,upload_to='servicios') # le indicamos que se guarde en la subcarpeta servicios
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
