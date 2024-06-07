d30a6489ce02636564afd45d93862a6546cb0659
Lisandro Leone <leonelisandro@gmail.com>
2024-06-07 13:38:44 -0300

    precio

--- proyecto/project/carro/context_processor.py
+++ proyecto/project/carro/context_processor.py
@@ -1,7 +1,7 @@
 def importe_total_carro(request):
-    total = 150
-    if request.user.is_authenticated:
-        carro = request.session.get("carro", {})  # Obtener el diccionario de carro de la sesión
-        for key, value in carro.items():  # Iterar sobre las claves y valores del diccionario
-            total += float(value["precio"]) * value["cantidad"]  # Calcular el total
+    total = 0
+    #if request.user.is_authenticated:
+    carro = request.session.get("carro", {})  # Obtener el diccionario de carro de la sesión
+    for key, value in carro.items():  # Iterar sobre las claves y valores del diccionario
+        total += float(value["precio"]) # Calcular el total
     return {"importe_total_carro": total}
